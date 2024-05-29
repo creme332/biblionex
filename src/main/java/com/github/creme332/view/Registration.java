c7e61f066df02f59b06ffa2ab629440651cf8835
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-29 14:37:49 +0400

    add sample registration page

---
+++ src/main/java/com/github/creme332/view/Registration.java
@@ -0,0 +1,68 @@
+package com.github.creme332.view;
+
+import javax.swing.*;
+
+public class Registration extends JPanel {
+    private JTextField userText;
+    private JPasswordField passwordText;
+    private JLabel success;
+    private JButton registerButton;
+
+    public Registration() {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel heading = new JLabel("Registration");
+        add(heading);
+
+        JPanel container = new JPanel();
+        container.setLayout(null);
+        add(container);
+
+        // Create JLabel for username
+        JLabel userLabel = new JLabel("Email");
+        userLabel.setBounds(10, 20, 80, 25);
+        container.add(userLabel);
+
+        // Create text field for username
+        userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        container.add(userText);
+
+        // Create JLabel for password
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        container.add(passwordLabel);
+
+        // Create text field for password
+        passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 50, 165, 25);
+        container.add(passwordText);
+
+        // Create Register button
+        registerButton = new JButton("Register");
+        registerButton.setBounds(10, 80, 100, 25);
+        container.add(registerButton);
+
+        // Create success message label
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        container.add(success);
+    }
+
+    public JButton getRegisterButton() {
+        return registerButton;
+    }
+
+    public String getEnteredText() {
+        return userText.getText();
+
+    }
+
+    public char[] getEnteredUser() {
+        return passwordText.getPassword();
+    }
+
+    public void setErrorMessage(String text) {
+        success.setText(text);
+    }
+}
