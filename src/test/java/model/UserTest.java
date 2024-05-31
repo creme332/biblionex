5a3f8af055ea16152c014280324a5186869b99cc
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-31 13:24:58 +0400

    create initial test

---
+++ src/test/java/model/UserTest.java
@@ -0,0 +1,23 @@
+package model;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void initializeModel() {
+        assertTrue(true);
+
+    }
+}
