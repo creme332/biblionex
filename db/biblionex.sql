5bd992d7b8556ca3238deca3e06f890184aff332
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-03 19:10:27 +0400

    add create database statement

--- db/biblionex.sql
+++ db/biblionex.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `biblionex`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `biblionex` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `biblionex`;
+
+--
 -- Table structure for table `author`
 --
 
@@ -537,4 +545,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-03 18:41:57
+-- Dump completed on 2024-06-03 19:09:42
