ee8f027988024a5e12b6408221a72eb938097aef
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-06 21:07:44 +0400

    create custom theme

---
+++ src/main/java/com/github/creme332/view/MyTheme.java
@@ -0,0 +1,39 @@
+package com.github.creme332.view;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.io.InputStream;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+public class MyTheme extends FlatDarculaLaf {
+    public static boolean setup() {
+        installFonts();
+        // look for properties file in theme folder (relative to resource)
+        MyTheme.registerCustomDefaultsSource("theme");
+        return setup(new MyTheme());
+    }
+
+    @Override
+    public String getName() {
+        return "TizcTheme";
+    }
+
+    private static void installFonts() {
+        InputStream inputStream;
+        String[] fontStyle = { "Regular" };
+
+        for (String style : fontStyle) {
+            try {
+                String path = String.format("/font/Poppins-%s.ttf", style);
+                inputStream = MyTheme.class.getResourceAsStream(path);
+                GraphicsEnvironment.getLocalGraphicsEnvironment()
+                        .registerFont(Font.createFont(Font.TRUETYPE_FONT, inputStream));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
