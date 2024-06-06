d7e17ed4222568e562660c4937920a0e4b30f182
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-06 21:09:31 +0400

    replace DAO with static methods

--- src/main/java/com/github/creme332/model/Librarian.java
+++ src/main/java/com/github/creme332/model/Librarian.java
@@ -1,5 +1,13 @@
 package com.github.creme332.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.github.creme332.utils.DatabaseConnection;
+
 public class Librarian extends User {
     private String role;
 
@@ -29,9 +37,107 @@ public class Librarian extends User {
         this.role = role;
     }
 
+    public static void save(Librarian librarian) {
+        final Connection conn = DatabaseConnection.getConnection();
+
+        String query = "INSERT INTO librarian (address, password, last_name, first_name, phone_no, email) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+            preparedStatement.setString(1, librarian.getAddress());
+            preparedStatement.setString(2, librarian.getPassword());
+            preparedStatement.setString(3, librarian.getLastName());
+            preparedStatement.setString(4, librarian.getFirstName());
+            preparedStatement.setString(5, librarian.getPhoneNo());
+            preparedStatement.setString(6, librarian.getEmail());
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new librarian was inserted successfully!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle exceptions (e.g., log the error or rethrow it)
+        }
+    }
+
+    public static void update(Librarian book) {
+        final Connection conn = DatabaseConnection.getConnection();
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    public static void delete(int id) {
+        final Connection conn = DatabaseConnection.getConnection();
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    public static Librarian findById(int id) {
+        final Connection conn = DatabaseConnection.getConnection();
+
+        Librarian librarian = null;
+        String query = "SELECT * FROM librarian WHERE librarian_id = ?";
+
+        try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                librarian = new Librarian();
+                librarian.setUserId(resultSet.getInt("librarian_id"));
+                librarian.setAddress(resultSet.getString("address"));
+                librarian.setPassword(resultSet.getString("password"));
+                librarian.setLastName(resultSet.getString("last_name"));
+                librarian.setFirstName(resultSet.getString("first_name"));
+                librarian.setPhoneNo(resultSet.getString("phone_no"));
+                librarian.setUserId(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle exceptions (e.g., log the error or rethrow it)
+        }
+
+        return librarian;
+    }
+
+    public static List<Librarian> findAll() {
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    public static Librarian findByEmail(String email) {
+        final Connection conn = DatabaseConnection.getConnection();
+
+        Librarian librarian = null;
+        String query = "SELECT * FROM librarian WHERE email = ?";
+
+        try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                librarian = new Librarian();
+                librarian.setUserId(resultSet.getInt("librarian_id"));
+                librarian.setAddress(resultSet.getString("address"));
+                librarian.setPassword(resultSet.getString("password"));
+                librarian.setLastName(resultSet.getString("last_name"));
+                librarian.setFirstName(resultSet.getString("first_name"));
+                librarian.setPhoneNo(resultSet.getString("phone_no"));
+                librarian.setEmail(email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle exceptions (e.g., log the error or rethrow it)
+        }
+
+        return librarian;
+    }
+
     @Override
     public String toString() {
         return "Librarian{" +
+
                 "librarian_id=" + userId +
                 ", address='" + address + '\'' +
                 ", password='" + password + '\'' +
--- src/main/java/com/github/creme332/model/Patron.java
+++ src/main/java/com/github/creme332/model/Patron.java
@@ -1,6 +1,13 @@
 package com.github.creme332.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
+
+import com.github.creme332.utils.DatabaseConnection;
 
 public class Patron extends User {
     private Date registrationDate;
@@ -10,7 +17,56 @@ public class Patron extends User {
     public Patron(String email, String password, int userId, String address, String firstName, String lastName,
             String phoneNo) {
         super(email, password, userId, address, firstName, lastName, phoneNo);
-        // TODO Auto-generated constructor stub
     }
 
+    public Patron() {
+        super();
+        creditCardNo = "";
+    }
+
+    public static void save(Patron librarian) {
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    public static void update(Patron librarian) {
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    public static void delete(int id) {
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    public static Patron findById(int id) {
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    public static Patron findByEmail(String email) {
+        final Connection conn = DatabaseConnection.getConnection();
+
+        Patron patron = null;
+        String query = "SELECT * FROM patron WHERE email = ?";
+
+        try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                patron = new Patron();
+                patron.setUserId(resultSet.getInt("librarian_id"));
+                patron.setAddress(resultSet.getString("address"));
+                patron.setPassword(resultSet.getString("password"));
+                patron.setLastName(resultSet.getString("last_name"));
+                patron.setFirstName(resultSet.getString("first_name"));
+                patron.setPhoneNo(resultSet.getString("phone_no"));
+                patron.setEmail(email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return patron;
+    }
+
+    public static List<Patron> findAll() {
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
 }
