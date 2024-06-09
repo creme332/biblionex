08d8b4b88dc58044b214d54252a2e4be848bc5bc
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-09 19:43:47 +0400

    simplify

--- src/main/java/com/github/creme332/model/UserType.java
+++ src/main/java/com/github/creme332/model/UserType.java
@@ -1,16 +1,6 @@
 package com.github.creme332.model;
 
 public enum UserType {
-    PATRON("PATRON"),
-    LIBRARIAN("LIBRARIAN");
-
-    private final String type;
-
-    UserType(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
+    PATRON,
+    LIBRARIAN;
 }
