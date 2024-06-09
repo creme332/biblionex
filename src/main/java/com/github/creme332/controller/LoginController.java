320855da6219bb894f7db7c936a39da7aa36485f
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-09 19:39:11 +0400

    rework based on latest changes to model

--- src/main/java/com/github/creme332/controller/LoginController.java
+++ src/main/java/com/github/creme332/controller/LoginController.java
@@ -2,12 +2,12 @@ package com.github.creme332.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 
 import com.github.creme332.model.AppState;
 import com.github.creme332.model.Librarian;
 import com.github.creme332.model.Patron;
 import com.github.creme332.model.User;
+import com.github.creme332.model.UserType;
 import com.github.creme332.view.Login;
 
 /**
@@ -28,59 +28,37 @@ public class LoginController {
             public void actionPerformed(ActionEvent e) {
                 String email = loginPage.getEmail();
 
-                if (authenticateUser(email, loginPage.getPassword())) {
-                    loginPage.hideError();
+                // assuming is a patron, find his account
+                User user = Patron.findByEmail(email);
 
-                    if (userType.equals("patron")) {
-                        app.setLoggedInUser(Patron.findByEmail(email));
-                        app.setCurrentScreen(Screen.PATRON_DASHBOARD_SCREEN);
-                    } else {
-                        app.setLoggedInUser(Librarian.findByEmail(email));
-                        app.setCurrentScreen(Screen.LIBRARIAN_DASHBOARD_SCREEN);
-                    }
-
-                } else {
-                    // invalid credentials
-                    loginPage.showError();
+                // if no patron account found, assume that user is librarian
+                if (user == null) {
+                    user = Librarian.findByEmail(email);
                 }
+
+                // if email invalid, show error
+                if (user == null) {
+                    loginPage.showError();
+                    return;
+                }
+
+                // validate password
+                if (!user.authenticate(email, loginPage.getPassword())) {
+                    loginPage.showError();
+                    return;
+                }
+
+                if (user.getUserType() == UserType.PATRON) {
+                    app.setLoggedInUser(Patron.findByEmail(email));
+                    app.setCurrentScreen(Screen.PATRON_DASHBOARD_SCREEN);
+                } else {
+                    app.setLoggedInUser(Librarian.findByEmail(email));
+                    app.setCurrentScreen(Screen.LIBRARIAN_DASHBOARD_SCREEN);
+                }
+
             }
         });
 
-        loginPage.getRegisterButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                app.setCurrentScreen(Screen.PATRON_REGISTRATION_SCREEN);
-            }
-        });
-    }
-
-    public boolean authenticateUser(String email, char[] enteredPassword) {
-        boolean isCorrect = true;
-
-        // check if user is a valid librarian
-        User user = Librarian.findByEmail(email);
-        if (user == null) {
-            // check if user is a patron
-            user = Patron.findByEmail(email);
-            if (user == null) {
-                Arrays.fill(enteredPassword, '0');
-                return false;
-            }
-            userType = "patron";
-        }
-
-        // verify passwords
-        char[] correctPassword = user.getPassword().toCharArray();
-        if (enteredPassword.length != correctPassword.length) {
-            isCorrect = false;
-        } else {
-            isCorrect = Arrays.equals(enteredPassword, correctPassword);
-        }
-
-        // Zero out the password for security purposes.
-        Arrays.fill(correctPassword, '0');
-        Arrays.fill(enteredPassword, '0');
-
-        return isCorrect;
+        loginPage.getRegisterButton().addActionListener(e -> app.setCurrentScreen(Screen.PATRON_REGISTRATION_SCREEN));
     }
 }
