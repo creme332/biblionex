21844731104c445564dfb45edf5c9e6f6ce96d00
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-11 17:21:11 +0400

    refactor

--- src/main/java/com/github/creme332/view/Login.java
+++ src/main/java/com/github/creme332/view/Login.java
@@ -12,6 +12,9 @@ import javax.swing.border.LineBorder;
 import org.kordamp.ikonli.swing.FontIcon;
 import org.kordamp.ikonli.bootstrapicons.BootstrapIcons;
 
+/**
+ * Login form for both patron and admin.
+ */
 public class Login extends JPanel {
     private JTextField emailField;
     private JPasswordField passwordField;
@@ -28,14 +31,7 @@ public class Login extends JPanel {
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
         add(main);
 
-        JPanel imgContainer = new JPanel(new GridBagLayout());
-        imgContainer.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
-
-        JLabel image = new JLabel();
-        FontIcon icon = FontIcon.of(BootstrapIcons.PERSON_CIRCLE, 200);
-        icon.setIconColor(Color.white);
-        image.setIcon(icon);
-        imgContainer.add(image);
+        JPanel imgContainer = getImgContainer();
         main.add(imgContainer);
 
         // add gap between imgContainer and formContainer
@@ -43,18 +39,18 @@ public class Login extends JPanel {
 
         JPanel formContainer = new JPanel();
         formContainer.setLayout(new BoxLayout(formContainer, BoxLayout.Y_AXIS));
-        formContainer.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
+        formContainer.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         main.add(formContainer);
 
         JLabel emailLabel = new JLabel("Email");
-        emailLabel.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
+        emailLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         formContainer.add(emailLabel);
 
         // Create text field for email
         emailField = new JTextField(20);
-        emailField.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
+        emailField.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         formContainer.add(emailField);
 
@@ -62,33 +58,27 @@ public class Login extends JPanel {
 
         // Create JLabel for password
         JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
+        passwordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         formContainer.add(passwordLabel);
 
         // Create text field for password
         passwordField = new JPasswordField(20);
-        passwordField.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
+        passwordField.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         formContainer.add(passwordField);
 
         formContainer.add(Box.createRigidArea(new Dimension(0, 60)));
 
         // Create login button
-        JPanel buttonContainer = new JPanel(new GridBagLayout());
-        buttonContainer.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
-
-        loginButton = new JButton("Login");
-
-        loginButton.setPreferredSize(new Dimension(500, 50));
-        buttonContainer.add(loginButton);
+        JPanel buttonContainer = getLoginButtonContainer();
         formContainer.add(buttonContainer);
 
         formContainer.add(Box.createRigidArea(new Dimension(0, 20)));
 
         // Create registration button
         registerButton = new JButton("Create a new account");
-        registerButton.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
+        registerButton.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         registerButton.setBorderPainted(false);
         registerButton.setFocusPainted(false);
@@ -115,5 +105,28 @@ public class Login extends JPanel {
     public void showError() {
         emailField.setBorder(redBorder);
         passwordField.setBorder(redBorder);
+    }
+
+    private JPanel getLoginButtonContainer() {
+        JPanel buttonContainer = new JPanel(new GridBagLayout());
+        buttonContainer.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        loginButton = new JButton("Login");
+
+        loginButton.setPreferredSize(new Dimension(500, 50));
+        buttonContainer.add(loginButton);
+        return buttonContainer;
+    }
+
+    private JPanel getImgContainer() {
+        JPanel imgContainer = new JPanel(new GridBagLayout());
+        imgContainer.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        JLabel image = new JLabel();
+        FontIcon icon = FontIcon.of(BootstrapIcons.PERSON_CIRCLE, 200);
+        icon.setIconColor(Color.white);
+        image.setIcon(icon);
+        imgContainer.add(image);
+        return imgContainer;
     }
 }
