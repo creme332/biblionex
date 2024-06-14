f585d0232aa12db8ddf1e857b0d417c1bf99f12f
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-14 21:20:35 +0400

    add icon

--- src/main/java/com/github/creme332/view/SplashScreen.java
+++ src/main/java/com/github/creme332/view/SplashScreen.java
@@ -3,17 +3,28 @@ package com.github.creme332.view;
 import java.awt.*;
 import javax.swing.*;
 
+import org.kordamp.ikonli.bootstrapicons.BootstrapIcons;
+import org.kordamp.ikonli.swing.FontIcon;
+
 public class SplashScreen extends JPanel {
     JLabel text = new JLabel("biblionex");
+    GridBagConstraints gbc = new GridBagConstraints();
 
     SplashScreen() {
         setLayout(new GridBagLayout());
 
-        text.putClientProperty("FlatLaf.style", "font: 340% $large.font");
-        GridBagConstraints gbc = new GridBagConstraints();
+        text.putClientProperty("FlatLaf.style", "font: 260% $large.font");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        this.add(text, gbc);
+
+        FontIcon icon = FontIcon.of(BootstrapIcons.BOOK, 120);
+        icon.setIconColor(Color.white);
+        JLabel x = new JLabel();
+        x.setIcon(icon);
+
         gbc.gridx = 0;
         gbc.gridy = 0;
-
-        this.add(text);
+        this.add(x, gbc);
     }
 }
