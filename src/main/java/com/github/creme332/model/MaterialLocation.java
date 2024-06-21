d674329b54311d2f91643ae99d8b2bfcf15553b2
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-21 18:35:48 +0400

    remove barcode and material id

--- src/main/java/com/github/creme332/model/MaterialLocation.java
+++ src/main/java/com/github/creme332/model/MaterialLocation.java
@@ -1,35 +1,14 @@
 package com.github.creme332.model;
 
 public class MaterialLocation {
-
-    private int barcode;
-    private int materialId;
     private int shelfNo;
     private int aisleNo;
     private int sectionNo;
 
-    public MaterialLocation(int barcode, int materialId, int shelfNo, int aisleNo, int sectionNo) {
-        this.barcode = barcode;
-        this.materialId = materialId;
+    public MaterialLocation(int shelfNo, int aisleNo, int sectionNo) {
         this.shelfNo = shelfNo;
         this.aisleNo = aisleNo;
         this.sectionNo = sectionNo;
-    }
-
-    public int getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(int barcode) {
-        this.barcode = barcode;
-    }
-
-    public int getMaterialId() {
-        return materialId;
-    }
-
-    public void setMaterialId(int materialId) {
-        this.materialId = materialId;
     }
 
     public int getShelfNo() {
