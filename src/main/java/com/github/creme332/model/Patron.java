baead6cc8f4f063a20d0dbd8f1c04025f0a051a6
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-22 11:34:57 +0400

    add toString, process registration and birth date,

--- src/main/java/com/github/creme332/model/Patron.java
+++ src/main/java/com/github/creme332/model/Patron.java
@@ -17,11 +17,12 @@ public class Patron extends User {
     private Date birthDate;
 
     public Patron(String email, String password, int userId, String address, String firstName, String lastName,
-            String phoneNo, String creditCardNo, Date birthDate) {
+            String phoneNo, String creditCardNo, Date birthDate, Date registrationDate) {
         super(email, password, userId, address, firstName, lastName, phoneNo);
         userType = UserType.PATRON;
         this.creditCardNo = creditCardNo;
         this.birthDate = birthDate;
+        this.registrationDate = registrationDate;
     }
 
     public Patron(String email, String password, String address, String firstName, String lastName,
@@ -30,17 +31,31 @@ public class Patron extends User {
         userType = UserType.PATRON;
         this.creditCardNo = creditCardNo;
         this.birthDate = birthDate;
+        this.registrationDate = new Date();
     }
 
-    public Patron() {
-        super();
-        userType = UserType.PATRON;
-        creditCardNo = "";
-    }
-
+    /**
+     * Saves a patron to database. patron ID and registration date are automatically
+     * set by database.
+     * 
+     * @param patron
+     * @throws SQLException
+     */
     public static void save(Patron patron) throws SQLException {
         final Connection conn = DatabaseConnection.getConnection();
-        String query = "INSERT INTO patron (address, password, last_name, first_name, phone_no, email, credit_card_no) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String query = """
+                INSERT INTO patron (
+                    address,
+                    password,
+                    last_name,
+                    first_name,
+                    phone_no,
+                    email,
+                    credit_card_no,
+                    birth_date
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                 """;
 
         PasswordAuthentication passwordAuthentication = new PasswordAuthentication();
         String hashedPassword = passwordAuthentication.hash(patron.getPassword().toCharArray());
@@ -53,26 +68,40 @@ public class Patron extends User {
             preparedStatement.setString(5, patron.getPhoneNo());
             preparedStatement.setString(6, patron.getEmail());
             preparedStatement.setString(7, patron.getCreditCardNo());
+            preparedStatement.setDate(8, new java.sql.Date(patron.getBirthDate().getTime()));
             preparedStatement.executeUpdate();
         }
     }
 
+    /**
+     * Updates all patron attributes except patron ID, password, registration date.
+     * 
+     * @throws SQLException
+     */
     public static void update(Patron patron) throws SQLException {
         final Connection conn = DatabaseConnection.getConnection();
-        String query = "UPDATE patron SET address = ?, password = ?, last_name = ?, first_name = ?, phone_no = ?, email = ? WHERE patron_id = ?";
+        String query = """
+                UPDATE patron
+                SET address = ?,
+                    last_name = ?,
+                    first_name = ?,
+                    phone_no = ?,
+                    email = ?,
+                    birth_date = ?,
+                    credit_card_no = ?
+                WHERE patron_id = ?
+                """;
 
-        PasswordAuthentication passwordAuthentication = new PasswordAuthentication();
-        String hashedPassword = passwordAuthentication.hash(patron.getPassword().toCharArray());
-
-        try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-            preparedStatement.setString(1, patron.getAddress());
-            preparedStatement.setString(2, hashedPassword); // Save hashed password
-            preparedStatement.setString(3, patron.getLastName());
-            preparedStatement.setString(4, patron.getFirstName());
-            preparedStatement.setString(5, patron.getPhoneNo());
-            preparedStatement.setString(6, patron.getEmail());
-            preparedStatement.setInt(7, patron.getUserId());
-            preparedStatement.executeUpdate();
+        try (PreparedStatement updatePatron = conn.prepareStatement(query)) {
+            updatePatron.setString(1, patron.getAddress());
+            updatePatron.setString(2, patron.getLastName());
+            updatePatron.setString(3, patron.getFirstName());
+            updatePatron.setString(4, patron.getPhoneNo());
+            updatePatron.setString(5, patron.getEmail());
+            updatePatron.setDate(6, new java.sql.Date(patron.getBirthDate().getTime()));
+            updatePatron.setString(7, patron.getCreditCardNo());
+            updatePatron.setInt(8, patron.getUserId());
+            updatePatron.executeUpdate();
         }
     }
 
@@ -96,14 +125,17 @@ public class Patron extends User {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
-                patron = new Patron();
-                patron.setUserId(resultSet.getInt("patron_id"));
-                patron.setAddress(resultSet.getString("address"));
-                patron.setPassword(resultSet.getString("password"));
-                patron.setLastName(resultSet.getString("last_name"));
-                patron.setFirstName(resultSet.getString("first_name"));
-                patron.setPhoneNo(resultSet.getString("phone_no"));
-                patron.setEmail(resultSet.getString("email"));
+                patron = new Patron(
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        resultSet.getInt("patron_id"),
+                        resultSet.getString("address"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("phone_no"),
+                        resultSet.getString("credit_card_no"),
+                        resultSet.getDate("birth_date"),
+                        resultSet.getDate("registration_date"));
             }
         }
         return patron;
@@ -120,14 +152,17 @@ public class Patron extends User {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
-                patron = new Patron();
-                patron.setUserId(resultSet.getInt("patron_id"));
-                patron.setAddress(resultSet.getString("address"));
-                patron.setPassword(resultSet.getString("password"));
-                patron.setLastName(resultSet.getString("last_name"));
-                patron.setFirstName(resultSet.getString("first_name"));
-                patron.setPhoneNo(resultSet.getString("phone_no"));
-                patron.setEmail(email);
+                patron = new Patron(
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        resultSet.getInt("patron_id"),
+                        resultSet.getString("address"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("phone_no"),
+                        resultSet.getString("credit_card_no"),
+                        resultSet.getDate("birth_date"),
+                        resultSet.getDate("registration_date"));
             }
         }
         return patron;
@@ -142,14 +177,17 @@ public class Patron extends User {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                Patron patron = new Patron();
-                patron.setUserId(resultSet.getInt("patron_id"));
-                patron.setAddress(resultSet.getString("address"));
-                patron.setPassword(resultSet.getString("password"));
-                patron.setLastName(resultSet.getString("last_name"));
-                patron.setFirstName(resultSet.getString("first_name"));
-                patron.setPhoneNo(resultSet.getString("phone_no"));
-                patron.setEmail(resultSet.getString("email"));
+                Patron patron = new Patron(
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        resultSet.getInt("patron_id"),
+                        resultSet.getString("address"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("phone_no"),
+                        resultSet.getString("credit_card_no"),
+                        resultSet.getDate("birth_date"),
+                        resultSet.getDate("registration_date"));
                 patrons.add(patron);
             }
         }
@@ -162,5 +200,37 @@ public class Patron extends User {
 
     public void setCreditCardNo(String creditCardNo) {
         this.creditCardNo = creditCardNo;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setRegistrationDate(Date newDate) {
+        this.registrationDate = newDate;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Patron{" +
+                "patron_id=" + userId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", last_name='" + lastName + '\'' +
+                ", first_name='" + firstName + '\'' +
+                ", phone_no='" + phoneNo + '\'' +
+                ", address='" + address + '\'' +
+                ", birth_date='" + birthDate + '\'' +
+                ", registration_date='" + registrationDate + '\'' +
+                ", credit_card_no='" + creditCardNo + '\'' +
+                '}';
     }
 }
