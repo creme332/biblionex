03b9500707b85ae06455f3feaace5d2a30643112
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-22 14:08:25 +0400

    remove string representation since material type is not stored in database

--- src/main/java/com/github/creme332/model/MaterialType.java
+++ src/main/java/com/github/creme332/model/MaterialType.java
@@ -1,31 +1,7 @@
 package com.github.creme332.model;
 
 public enum MaterialType {
-  VIDEO("video"),
-  JOURNAL("journal"),
-  BOOK("book");
-
-  String stringInDB;
-
-  /**
-   * 
-   * @param stringRep string representation of enum as in database
-   */
-  MaterialType(String stringRep) {
-    this.stringInDB = stringRep;
-  }
-
-  @Override
-  public String toString() {
-    return stringInDB;
-  }
-
-  public static MaterialType fromString(String str) {
-    for (MaterialType value : MaterialType.values()) {
-      if (value.toString().equals(str)) {
-        return value;
-      }
-    }
-    return MaterialType.valueOf(str);
-  }
+  VIDEO,
+  JOURNAL,
+  BOOK;
 }
