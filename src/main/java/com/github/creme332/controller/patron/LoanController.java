cfa738caf1733b4c587a53e0f081ce2b0730e170
creme332 <65414576+creme332@users.noreply.github.com>
2024-07-06 11:55:28 +0400

    rework controller

--- src/main/java/com/github/creme332/controller/patron/LoanController.java
+++ src/main/java/com/github/creme332/controller/patron/LoanController.java
@@ -1,47 +1,94 @@
 package com.github.creme332.controller.patron;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.swing.JOptionPane;
 
-import com.github.creme332.view.patron.Loan;
+import com.github.creme332.view.patron.LoanPage;
 import com.github.creme332.model.AppState;
-import com.github.creme332.model.Fine;
-import com.github.creme332.model.Fine.LoanFineData;
+import com.github.creme332.model.Loan;
 import com.github.creme332.model.Patron;
+import com.github.creme332.model.User;
+import com.github.creme332.model.UserType;
 
-public class LoanController {
-    private Loan loan;
-    private AppState app;
+/**
+ * Controller for patron loan page.
+ */
+public class LoanController implements PropertyChangeListener {
+    private LoanPage loanView;
+    /**
+     * Logged in patron. It is initialized when the loggedInUser in AppState
+     * changes.
+     */
+    Patron patron;
 
-    public LoanController(AppState app, Loan loan) {
-        this.loan = loan;
-        this.app = app;
-        loan.addLoanActionListener(e -> handlePayAction(Integer.parseInt(e.getActionCommand())));
-        loadData();
+    public LoanController(AppState app, LoanPage loanView) {
+        this.loanView = loanView;
+        app.addPropertyChangeListener(this);
+        loanView.addLoanActionListener(e -> handlePayment(Integer.parseInt(e.getActionCommand())));
     }
 
-    // fetch patron account
-    Patron patron = (Patron) app.getLoggedInUser();
-
-    public void loadData() {
+    public void displayLoans() {
+        List<Loan> loans;
         try {
-            List<LoanFineData> records = Fine.findLoanFineRecordsByPatronId(patron.getUserId());
-            loan.setLoanFineRecords(records);
+            loans = patron.getLoans();
+            loanView.updateTableModel(loans);
         } catch (SQLException e) {
+            JOptionPane.showMessageDialog(loanView,
+                    "Unable to fetch active loans from database. Please try again later.",
+                    "Database Error",
+                    JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
-            JOptionPane.showMessageDialog(loan, "Error loading data", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    public void handlePayAction(int loanId) {
+    public void handlePayment(int loanId) {
+        Loan currentLoan = null;
+
+        // Fetch loan details
         try {
-            Fine.deleteByLoanId(loanId);
-            loadData(); // Reload the data to refresh the table
+            currentLoan = Loan.findById(loanId);
         } catch (SQLException e) {
             e.printStackTrace();
-            JOptionPane.showMessageDialog(loan, "Error processing payment", "Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+
+        // Check if payment is needed
+        if (currentLoan.getAmountDue() <= 0) {
+            JOptionPane.showMessageDialog(loanView, "No payment required for this loan.", "Message",
+                    JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+
+        boolean paymentSuccessful = patron.payFine(currentLoan);
+        if (paymentSuccessful) {
+            JOptionPane.showMessageDialog(loanView, "Payment Successful.", "Error",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(loanView, "Payment is not possible yet.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        String propertyName = evt.getPropertyName();
+        if (propertyName.equals("loggedInUser")) {
+
+            // ignore event if logged in user is not a patron
+            User newUser = (User) evt.getNewValue();
+            if (newUser == null
+                    || newUser.getUserType() != UserType.PATRON)
+                return;
+
+            // initialize patron
+            patron = (Patron) newUser;
+
+            // display his loans
+            displayLoans();
         }
     }
 }
