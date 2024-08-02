108d70f4e15b62c5579d37bbb69c5daadf6b9b65
creme332 <65414576+creme332@users.noreply.github.com>
2024-08-02 21:28:52 +0400

    add custom toString

--- src/main/java/com/github/creme332/model/LoanStatus.java
+++ src/main/java/com/github/creme332/model/LoanStatus.java
@@ -3,5 +3,14 @@ package com.github.creme332.model;
 public enum LoanStatus {
     COMPLETE,
     OVERDUE,
-    PENDING
+    PENDING;
+
+    @Override
+    public String toString() {
+        // Get the name of the constant
+        String name = name();
+        
+        // Capitalize the first letter and make the rest lowercase
+        return name.substring(0, 1) + name.substring(1).toLowerCase();
+    }
 }
