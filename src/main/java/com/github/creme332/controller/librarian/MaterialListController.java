2bde11c72aef66dac2aa4f12be90c613e6955419
creme332 <65414576+creme332@users.noreply.github.com>
2024-08-05 13:18:45 +0400

    refactor

--- src/main/java/com/github/creme332/controller/librarian/MaterialListController.java
+++ src/main/java/com/github/creme332/controller/librarian/MaterialListController.java
@@ -6,6 +6,7 @@ import javax.swing.table.DefaultTableModel;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.event.DocumentEvent;
 
@@ -13,34 +14,58 @@ import com.github.creme332.controller.Screen;
 import com.github.creme332.model.AppState;
 import com.github.creme332.model.Book;
 import com.github.creme332.model.Journal;
-import com.github.creme332.model.Material;
 import com.github.creme332.model.User;
 import com.github.creme332.model.UserType;
 import com.github.creme332.model.Video;
 import com.github.creme332.utils.StringUtil;
 import com.github.creme332.model.Librarian;
+import com.github.creme332.model.Material;
+import com.github.creme332.model.MaterialType;
 import com.github.creme332.view.librarian.MaterialList;
+import com.github.creme332.view.patron.Catalog;
 
 public class MaterialListController implements PropertyChangeListener {
     private MaterialList view;
     private Librarian librarian;
+    private List<Material> allMaterials;
 
     public MaterialListController(AppState app, MaterialList view) {
         this.view = view;
         app.addPropertyChangeListener(this);
 
         view.getBackButton().addActionListener(e -> app.setCurrentScreen(Screen.LIBRARIAN_DASHBOARD_SCREEN));
-        view.getSearchButton().addActionListener(e -> searchMaterials());
         view.getNewMaterialButton().addActionListener(e -> app.setCurrentScreen(Screen.LIBRARIAN_CATALOGING_SCREEN));
 
         Thread th = new Thread() {
             @Override
             public void run() {
-                refreshTable();
+                fetchMaterials();
+                displayMaterials(allMaterials);
                 addTableButtonListeners();
             }
         };
         th.start();
+    }
+
+    /**
+     * Fetch all materials from database and saves it to allMaterials.
+     */
+    private void fetchMaterials() {
+        try {
+            allMaterials = Material.findAllMaterials();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(view, "Error occurred while fetching materials.", "Database Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private Material findMaterialById(int id) {
+        for (Material material : allMaterials) {
+            if (material.getMaterialId() == id) {
+                return material;
+            }
+        }
+        return null; // Return null if no material with the given ID is found
     }
 
     private void addTableButtonListeners() {
@@ -50,17 +75,17 @@ public class MaterialListController implements PropertyChangeListener {
         view.getSearchField().getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
-                searchMaterials();
+                filterMaterials();
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
-                searchMaterials();
+                filterMaterials();
             }
 
             @Override
             public void changedUpdate(DocumentEvent e) {
-                searchMaterials();
+                filterMaterials();
             }
         });
 
@@ -68,7 +93,7 @@ public class MaterialListController implements PropertyChangeListener {
             int selectedRow = table.getSelectedRow();
             if (selectedRow != -1) {
                 int materialId = (int) table.getValueAt(selectedRow, 0);
-                handleViewMaterial(materialId);
+                Catalog.showMaterialDialog(findMaterialById(materialId), table);
             }
         });
 
@@ -81,235 +106,78 @@ public class MaterialListController implements PropertyChangeListener {
         });
     }
 
-    private void refreshTable() {
+    private void displayMaterials(List<Material> materialsToDisplay) {
         DefaultTableModel tableModel = view.getTableModel();
         tableModel.setRowCount(0);
 
-        try {
-            List<Book> books = Book.findAll();
-            List<Journal> journals = Journal.findAll();
-            List<Video> videos = Video.findAll();
-
-            for (Book book : books) {
-                Object[] rowData = {
-                        book.getMaterialId(),
-                        book.getTitle(),
-                        book.getPublisherId(),
-                        book.getDescription(),
-                        book.getAgeRestriction(),
-                        "Book"
-                };
-                tableModel.addRow(rowData);
-            }
-
-            for (Journal journal : journals) {
-                Object[] rowData = {
-                        journal.getMaterialId(),
-                        journal.getTitle(),
-                        journal.getPublisherId(),
-                        journal.getDescription(),
-                        journal.getAgeRestriction(),
-                        "Journal"
-                };
-                tableModel.addRow(rowData);
-            }
-
-            for (Video video : videos) {
-                Object[] rowData = {
-                        video.getMaterialId(),
-                        video.getTitle(),
-                        video.getPublisherId(),
-                        video.getDescription(),
-                        video.getAgeRestriction(),
-                        "Video"
-                };
-                tableModel.addRow(rowData);
-            }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(view, "Error occurred while fetching materials.", "Database Error",
-                    JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private void handleViewMaterial(int materialId) {
-        try {
-            Material material = null;
-
-            // Try to find the material by its ID in the respective lists
-            for (Book book : Book.findAll()) {
-                if (book.getMaterialId() == materialId) {
-                    material = book;
-                    break;
-                }
-            }
-
-            if (material == null) {
-                for (Journal journal : Journal.findAll()) {
-                    if (journal.getMaterialId() == materialId) {
-                        material = journal;
-                        break;
-                    }
-                }
-            }
-
-            if (material == null) {
-                for (Video video : Video.findAll()) {
-                    if (video.getMaterialId() == materialId) {
-                        material = video;
-                        break;
-                    }
-                }
-            }
-
-            if (material == null) {
-                JOptionPane.showMessageDialog(view, "Material not found.", "Error", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-
-            String title = "";
-            String details = "";
-
-            if (material instanceof Book) {
-                Book book = (Book) material;
-                title = "Book: " + book.getTitle();
-                details = "<b style='color:blue;'>Title:</b> " + book.getTitle() + "<br>"
-                        + "<b style='color:blue;'>ISBN:</b> " + book.getIsbn() + "<br>"
-                        + "<b style='color:blue;'>Page Count:</b> " + book.getPageCount() + "<br>"
-                        + "<b style='color:blue;'>Description:</b> " + book.getDescription() + "<br>"
-                        + "<b style='color:blue;'>Publisher ID:</b> " + book.getPublisherId() + "<br>"
-                        + "<b style='color:blue;'>Age Restriction:</b> " + book.getAgeRestriction();
-            } else if (material instanceof Video) {
-                Video video = (Video) material;
-                title = "Video: " + video.getTitle();
-                details = "<b style='color:blue;'>Title:</b> " + video.getTitle() + "<br>"
-                        + "<b style='color:blue;'>Language:</b> " + video.getLanguage() + "<br>"
-                        + "<b style='color:blue;'>Duration:</b> " + video.getDuration() + " minutes<br>"
-                        + "<b style='color:blue;'>Rating:</b> " + video.getRating() + "<br>"
-                        + "<b style='color:blue;'>Format:</b> " + video.getFormat() + "<br>"
-                        + "<b style='color:blue;'>Description:</b> " + video.getDescription() + "<br>"
-                        + "<b style='color:blue;'>Publisher ID:</b> " + video.getPublisherId() + "<br>"
-                        + "<b style='color:blue;'>Age Restriction:</b> " + video.getAgeRestriction();
-            } else if (material instanceof Journal) {
-                Journal journal = (Journal) material;
-                title = "Journal: " + journal.getTitle();
-                details = "<b style='color:blue;'>Title:</b> " + journal.getTitle() + "<br>"
-                        + "<b style='color:blue;'>ISSN:</b> " + journal.getIssn() + "<br>"
-                        + "<b style='color:blue;'>Website:</b> " + journal.getWebsite() + "<br>"
-                        + "<b style='color:blue;'>Frequency:</b> " + journal.getFrequency() + "<br>"
-                        + "<b style='color:blue;'>Start Date:</b> " + journal.getStartDate() + "<br>"
-                        + "<b style='color:blue;'>Description:</b> " + journal.getDescription() + "<br>"
-                        + "<b style='color:blue;'>Publisher ID:</b> " + journal.getPublisherId() + "<br>"
-                        + "<b style='color:blue;'>Age Restriction:</b> " + journal.getAgeRestriction();
-            }
-
-            JOptionPane.showMessageDialog(view, "<html><h2>" + title + "</h2><p>" + details + "</p></html>",
-                    "Material Details", JOptionPane.INFORMATION_MESSAGE);
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(view, "Error occurred while retrieving material details.", "Database Error",
-                    JOptionPane.ERROR_MESSAGE);
+        for (Material material : materialsToDisplay) {
+            Object[] rowData = {
+                    material.getMaterialId(),
+                    material.getTitle(),
+                    material.getPublisherId(),
+                    material.getDescription(),
+                    material.getAgeRestriction(),
+                    material.getType()
+            };
+            tableModel.addRow(rowData);
         }
     }
 
     private void handleDeleteMaterial(int materialId) {
-        int confirmation = JOptionPane.showConfirmDialog(view, "Are you sure you want to delete this material?",
+        int confirmation = JOptionPane.showConfirmDialog(view,
+                String.format("Are you sure you want to delete material with ID %d?", materialId),
                 "Confirm Deletion", JOptionPane.YES_NO_OPTION);
-        if (confirmation == JOptionPane.YES_OPTION) {
-            try {
-                // Attempt to delete the material as a Book
-                Book book = Book.findById(materialId);
-                if (book != null) {
-                    Book.delete(book);
-                } else {
-                    // Attempt to delete the material as a Journal
-                    Journal journal = Journal.findById(materialId);
-                    if (journal != null) {
-                        Journal.delete(journal);
-                    } else {
-                        // Attempt to delete the material as a Video
-                        Video video = Video.findByID(materialId);
-                        if (video != null) {
-                            Video.delete(video);
-                        } else {
-                            JOptionPane.showMessageDialog(view, "Material not found.", "Error",
-                                    JOptionPane.ERROR_MESSAGE);
-                            return;
-                        }
-                    }
-                }
 
-                JOptionPane.showMessageDialog(view, "Material deleted successfully.", "Success",
-                        JOptionPane.INFORMATION_MESSAGE);
-                refreshTable();
-            } catch (SQLException e) {
-                JOptionPane.showMessageDialog(view, "Error occurred while deleting the material.", "Database Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
+        if (confirmation != JOptionPane.YES_OPTION)
+            return;
 
-    private void searchMaterials() {
-        String searchText = view.getSearchField().getText().trim();
-        DefaultTableModel tableModel = view.getTableModel();
-        tableModel.setRowCount(0);
+        Material material = findMaterialById(materialId);
 
-        if (searchText.isEmpty()) {
-            refreshTable(); // Reload all materials if the search field is empty
+        if (material == null) {
+            JOptionPane.showMessageDialog(view, String.format("Material with ID %d not found.", materialId), "Error",
+                    JOptionPane.ERROR_MESSAGE);
             return;
         }
 
         try {
-            List<Book> books = Book.findAll();
-            List<Journal> journals = Journal.findAll();
-            List<Video> videos = Video.findAll();
-
-            for (Book book : books) {
-                if (StringUtil.isSimilar(String.valueOf(book.getMaterialId()), searchText) ||
-                        StringUtil.isSimilar(book.getTitle(), searchText)) {
-                    Object[] rowData = {
-                            book.getMaterialId(),
-                            book.getTitle(),
-                            book.getPublisherId(),
-                            book.getDescription(),
-                            book.getAgeRestriction(),
-                            "Book"
-                    };
-                    tableModel.addRow(rowData);
-                }
+            if (material.getType() == MaterialType.BOOK) {
+                Book.delete((Book) material);
+            } else if (material.getType() == MaterialType.JOURNAL) {
+                Journal.delete((Journal) material);
+            }
+            if (material.getType() == MaterialType.VIDEO) {
+                Video.delete((Video) material);
             }
 
-            for (Journal journal : journals) {
-                if (StringUtil.isSimilar(String.valueOf(journal.getMaterialId()), searchText) ||
-                        StringUtil.isSimilar(journal.getTitle(), searchText)) {
-                    Object[] rowData = {
-                            journal.getMaterialId(),
-                            journal.getTitle(),
-                            journal.getPublisherId(),
-                            journal.getDescription(),
-                            journal.getAgeRestriction(),
-                            "Journal"
-                    };
-                    tableModel.addRow(rowData);
-                }
-            }
+            JOptionPane.showMessageDialog(view, "Material deleted successfully.", "Success",
+                    JOptionPane.INFORMATION_MESSAGE);
 
-            for (Video video : videos) {
-                if (StringUtil.isSimilar(String.valueOf(video.getMaterialId()), searchText) ||
-                        StringUtil.isSimilar(video.getTitle(), searchText)) {
-                    Object[] rowData = {
-                            video.getMaterialId(),
-                            video.getTitle(),
-                            video.getPublisherId(),
-                            video.getDescription(),
-                            video.getAgeRestriction(),
-                            "Video"
-                    };
-                    tableModel.addRow(rowData);
-                }
-            }
+            displayMaterials(allMaterials);
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(view, "Error occurred while searching materials.", "Database Error",
+            JOptionPane.showMessageDialog(view, "Error occurred while deleting the material.", "Database Error",
                     JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
         }
+
+    }
+
+    private void filterMaterials() {
+        String searchText = view.getSearchField().getText().trim();
+
+        if (searchText.isEmpty()) {
+            displayMaterials(allMaterials); // Display all materials if the search field is empty
+            return;
+        }
+
+        List<Material> filteredMaterials = new ArrayList<>();
+
+        for (Material material : allMaterials) {
+            if (StringUtil.isSimilar(material.getTitle(), searchText)) {
+                filteredMaterials.add(material);
+            }
+        }
+
+        displayMaterials(filteredMaterials);
     }
 
     @Override
@@ -318,7 +186,8 @@ public class MaterialListController implements PropertyChangeListener {
 
         if (propertyName.equals("currentScreen")
                 && (Screen) evt.getNewValue() == Screen.LIBRARIAN_MATERIAL_LIST_SCREEN) {
-            refreshTable();
+            fetchMaterials();
+            displayMaterials(allMaterials);
         }
 
         if (propertyName.equals("loggedInUser")) {
